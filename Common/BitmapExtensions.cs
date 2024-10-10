189735f5420c0bb9714f055fb118d8f12964bb88
Artem Drahun <a.o.drahun@student.khai.edu>
2024-10-10 20:35:24 +0300

    Practice 3

--- Common/BitmapExtensions.cs
+++ Common/BitmapExtensions.cs
@@ -68,4 +68,104 @@ public static class BitmapExtensions
     
     public static Bitmap Transform<T>(this Bitmap source, Func<Color, T> action) where T : INumber<T> =>
         source.Transform(color => ByteToColor(Convert.ToByte(action(color))));
+    
+    public static Bitmap AddSaltAndPepperNoise(this Bitmap image, double density)
+    {
+        var noisyImage = new Bitmap(image);
+        var rand = new Random();
+
+        var numNoisyPixels = (int)(density * image.Width * image.Height);
+
+        for (var i = 0; i < numNoisyPixels; i++)
+        {
+            var x = rand.Next(image.Width);
+            var y = rand.Next(image.Height);
+
+            var noiseColor = rand.NextDouble() < 0.5 ? Color.Black : Color.White;
+            noisyImage.SetPixel(x, y, noiseColor);
+        }
+
+        return noisyImage;
+    }
+    
+    public static Bitmap ApplyMeanFilter(this Bitmap image, int filterSize)
+    {
+        var result = new Bitmap(image.Width, image.Height);
+
+        var offset = filterSize / 2;
+        for (var y = offset; y < image.Height - offset; y++)
+        {
+            for (var x = offset; x < image.Width - offset; x++)
+            {
+                int sumR = 0, sumG = 0, sumB = 0;
+
+                for (var fy = -offset; fy <= offset; fy++)
+                {
+                    for (var fx = -offset; fx <= offset; fx++)
+                    {
+                        var pixel = image.GetPixel(x + fx, y + fy);
+
+                        sumR += pixel.R;
+                        sumG += pixel.G;
+                        sumB += pixel.B;
+                    }
+                }
+
+                var numPixels = filterSize * filterSize;
+                var avgR = sumR / numPixels;
+                var avgG = sumG / numPixels;
+                var avgB = sumB / numPixels;
+
+                result.SetPixel(x, y, Color.FromArgb(avgR, avgG, avgB));
+            }
+        }
+
+        return result;
+    }
+    
+    public static Bitmap ApplyMedianFilter(this Bitmap image, int filterSize)
+    {
+        var result = new Bitmap(image.Width, image.Height);
+
+        var offset = filterSize / 2;
+        for (var y = offset; y < image.Height - offset; y++)
+        {
+            for (var x = offset; x < image.Width - offset; x++)
+            {
+                var rValues = new int[filterSize * filterSize];
+                var gValues = new int[filterSize * filterSize];
+                var bValues = new int[filterSize * filterSize];
+
+                var index = 0;
+                
+                for (var fy = -offset; fy <= offset; fy++)
+                {
+                    for (var fx = -offset; fx <= offset; fx++)
+                    {
+                        var pixel = image.GetPixel(x + fx, y + fy);
+
+                        rValues[index] = pixel.R;
+                        gValues[index] = pixel.G;
+                        bValues[index] = pixel.B;
+                        index++;
+                    }
+                }
+                
+                var medianR = GetMedian(rValues);
+                var medianG = GetMedian(gValues);
+                var medianB = GetMedian(bValues);
+                
+                result.SetPixel(x, y, Color.FromArgb(medianR, medianG, medianB));
+            }
+        }
+
+        return result;
+    }
+
+    private static int GetMedian(int[] values)
+    {
+        Array.Sort(values);
+        var middle = values.Length / 2;
+        return values[middle];
+    }
 }
---
+++ Practice3/Program.cs
@@ -0,0 +1,129 @@
+﻿using Common;
+
+namespace Practice3;
+
+internal static class Program
+{
+    private const float M = 0.0f;
+    private const float VariantNumber = 3.0f;
+    private const string FilePath = @"E:\Projects\University\Multimedia\Course work\C#\Image\pes.jpg";
+
+    private static void Main()
+    {
+        var imagePositionOffset = 300;
+        var form = new Form();
+        form.AutoScroll = true;
+        var imagePositionY = 0;
+        form.Size = new Size(1280, 720);
+        
+        var img = new Bitmap(FilePath);
+        form.Controls.Add(CreateImagePanel(img, "Original"));
+        imagePositionY += imagePositionOffset;
+        
+        var random = new Random();
+        const float sigma = VariantNumber / 100;
+        Console.WriteLine(random.NextDouble() * sigma + M);
+        var additive = img.Transform(color =>
+        {
+            var r = Clamp(color.R + (int)(random.NextDouble() * sigma + M));
+            var g = Clamp(color.G + (int)(random.NextDouble() * sigma + M));
+            var b = Clamp(color.B + (int)(random.NextDouble() * sigma + M));
+            return Color.FromArgb(r, g, b);
+        });
+        form.Controls.Add(CreateImagePanel(additive, "Additive noise", 0, imagePositionY));
+        imagePositionY += imagePositionOffset;
+
+        Console.WriteLine(1 + sigma * (random.NextDouble() - 0.5));
+        var multiplicative = img.Transform(color =>
+        {
+            var noiseFactor = 1 + sigma * (random.NextDouble() - 0.5);
+            var r = Clamp((int)(color.R * noiseFactor));
+            var g = Clamp((int)(color.G * noiseFactor));
+            var b = Clamp((int)(color.B * noiseFactor));
+            return Color.FromArgb(r, g, b);
+        });
+        form.Controls.Add(CreateImagePanel(multiplicative, "Multiplicative noise", 0, imagePositionY));
+        imagePositionY += imagePositionOffset;
+
+        var density = 0.1 + MathF.Abs(VariantNumber - 14) / 100;
+        var saltAndPepper = img.AddSaltAndPepperNoise(density);
+        form.Controls.Add(CreateImagePanel(saltAndPepper, "Salt & Pepper", 0, imagePositionY));
+        imagePositionY += imagePositionOffset;
+
+        int[] windowSizes = [3, 5, 7, 11];
+        foreach (var windowSize in windowSizes)
+        {
+            form.Controls.Add(MeanFilterImage(additive, windowSize, $"Additive noise mean {windowSize}x{windowSize}",
+                imagePositionY));
+            imagePositionY += imagePositionOffset;
+            form.Controls.Add(MeanFilterImage(multiplicative, windowSize,
+                $"Multiplicative noise mean {windowSize}x{windowSize}", imagePositionY));
+            imagePositionY += imagePositionOffset;
+            form.Controls.Add(MeanFilterImage(saltAndPepper, windowSize,
+                $"Salt & Pepper noise mean {windowSize}x{windowSize}", imagePositionY));
+            imagePositionY += imagePositionOffset;
+        }
+        
+        foreach (var windowSize in windowSizes)
+        {
+            form.Controls.Add(MedianFilterImage(additive, windowSize, $"Additive noise median {windowSize}x{windowSize}",
+                imagePositionY));
+            imagePositionY += imagePositionOffset;
+            form.Controls.Add(MedianFilterImage(multiplicative, windowSize,
+                $"Multiplicative noise median {windowSize}x{windowSize}", imagePositionY));
+            imagePositionY += imagePositionOffset;
+            form.Controls.Add(MedianFilterImage(saltAndPepper, windowSize,
+                $"Salt & Pepper noise median {windowSize}x{windowSize}", imagePositionY));
+            imagePositionY += imagePositionOffset;
+        }
+        
+        Application.Run(form);
+    }
+
+    private static Panel MeanFilterImage(Bitmap image, int windowSize, string text, int position)
+    {
+        var result = image.ApplyMeanFilter(windowSize);
+        return CreateImagePanel(result, text, 0, position);
+    }
+    private static Panel MedianFilterImage(Bitmap image, int windowSize, string text, int position)
+    {
+        var result = image.ApplyMedianFilter(windowSize);
+        return CreateImagePanel(result, text, 0, position);
+    }
+    
+    private static int Clamp(int value)
+    {
+        return Math.Max(0, Math.Min(255, value));
+    }
+
+    private static Panel CreateImagePanel(Bitmap image, string text, int x = 0, int y = 0)
+    {
+        var panel = new Panel
+        {
+            Controls =
+            {
+                new Label
+                {
+                    Text = text,
+                    Location = new Point(0, 0),
+                    Size = new Size(image.Width + 50, 25),
+                    TextAlign = ContentAlignment.MiddleCenter,
+                    Font = new Font("Arial", 7, FontStyle.Bold)
+                },
+                new PictureBox
+                {
+                    Image = image,
+                    SizeMode = PictureBoxSizeMode.Normal,
+                    Size = new Size(image.Width, image.Height),
+                    Location = new Point(5, 25),
+                }
+            },
+            Size = new Size(300, 280),
+            Location = new Point(x, y),
+            BorderStyle = BorderStyle.FixedSingle,
+            Padding = new Padding(10),
+            Margin = new Padding(10)
+        };
+        return panel;
+    }
+}
